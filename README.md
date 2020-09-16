6b7cecb82c123e95c4eb9004e09605eb3d196887
Richard Krasso <rkrasso@bellevue.edu>
2020-09-16 13:42:53 -0500

    Project upgrades/replacements

--- README.md
+++
@@ -1,2 +0,0 @@
-# web-450-code-snippets
-Mastering the MEAN Stack Bootcamp Code Snippets
--- examples/mongoose-node-example/db-models/role.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-let roleSchema = mongoose.Schema({
-  name: {type: String, unique: true, dropDups: true}
-});
-
-module.exports = mongoose.model('Role', roleSchema);

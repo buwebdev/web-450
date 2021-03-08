1d41aa1e1a52f96795b96361ab77008791ec4d02
Richard Krasso <rkrasso@bellevue.edu>
2021-03-08 10:59:55 -0600

    Updated bcrs angular version to 11.

--- starter-projects/bcrs/src/app/app-routing.module.ts
+++ starter-projects/bcrs/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true, enableTracing: false, scrollPositionRestoration: 'enabled'})],
+  imports: [RouterModule.forRoot(routes, { useHash: true, enableTracing: false, scrollPositionRestoration: 'enabled', relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
